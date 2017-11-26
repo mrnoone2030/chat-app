14936ce58e4404a1a8f614fdff32579dd9180810
Debanjan <debanjanevergreen@gmail.com>
2017-11-26 19:55:55 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
-const port = process.env.port || 3004;
+const port = process.env.PORT || 3004;
 
 app.use(express.static(publicPath));
 app.listen(port, () => {
